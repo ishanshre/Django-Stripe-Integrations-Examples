7e91e52cd23ee2f0248d2b88d191468af1d55663
Ishan Shrestha <ishan.shrestha356@gmail.com>
2023-01-19 08:24:03 +0545

    feat:  new project, models, view and templates     - setup new django project     - product models     - products list and details     - base templates

---
+++ djangoStripe3/core/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.urls import reverse
+
+from django.core.validators import MinValueValidator
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    thumbnail = models.ImageField(upload_to="product/image", blank=True)
+    url = models.URLField()
+    price = models.DecimalField(decimal_places=2, max_digits=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} --> {self.price}"
+    
+    # def get_absolute_url(self):
+    #     return reverse("core:detail", args=[self.id])
---
+++ djangoStripe3/core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+
+from core.models import Product
+
+
+class IndexView(ListView):
+    model = Product
+    context_object_name = 'products'
+    template_name = "index.html"
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'product/detail.html'
